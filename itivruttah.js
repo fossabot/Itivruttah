ebb2ea006cc307a9e824acd6620d5ed289387e54
Vasanth Developer <vasanthdevelopers@gmail.com>
2019-01-17 23:19:55 +0000

    Now exports logInfo function
    1. Created package-lock.json file
    2. Added chalk and dateformat as dependencies

--- itivruttah.js
+++ itivruttah.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
 'use strict';
+
+// Require the dependency modules
+const chalk = require('chalk'),
+      dateformat = require('dateformat');
+
+// The function that logs an info message to the console
+function logInfo(messageStr) {
+    console.log("[" + chalk.bold.blue("INFO") + " " + dateformat(new Date(), "hh:MM:ss") + "]: " + messageStr);
+}
+
+// Export the required modules
+module.exports = {
+    info: logInfo
+}
