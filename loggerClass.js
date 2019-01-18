b44ab742ccdea005e411180f4b0b597439494369
Vasanth Developer <vasanthdevelopers@gmail.com>
2019-01-18 17:44:11 +0000

    The messages are now rendered using renderTheme method
    1. Created _renderTheme function in loggerClass.js script file that replaces the placeholders with values

--- loggerClass.js
+++ loggerClass.js
@@ -4,7 +4,6 @@
 const dateformat = require('dateformat'),
       chalk = require('chalk');
 
-
 // Create the loggerClass
 class LoggerClass {
 
@@ -21,34 +20,52 @@ class LoggerClass {
         this.successStyle = chalk.bold.greenBright;
     }
 
+    // The private function that renders the theme
+    _renderTheme(strThemeTemplate, strType, strMessage) {
+        // Render the $DATE variable
+        strThemeTemplate = strThemeTemplate.replace("$DATE", dateformat(new Date(), "dd-mm-yyyy"));
+
+        // Render the $TIME variable
+        strThemeTemplate = strThemeTemplate.replace("$TIME", dateformat(new Date(), "hh:MM:ss"));
+
+        // Render the $TYPE variable [Colored for now]
+        strThemeTemplate = strThemeTemplate.replace("$TYPE", strType);
+
+        // Render the $MESSAGE variable
+        strThemeTemplate = strThemeTemplate.replace("$MESSAGE", strMessage);
+        
+        // Return the rendered string
+        return strThemeTemplate;
+    }
+
     // The function that logs a critical statement, sometimes called a fatal or error
     logCritical(strMessage) {
-        console.log(this.criticalStyle(`[ERROR]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.criticalStyle("CRIT"), strMessage));
     }
 
     // The function that logs a warning statement
     logWarning(strMessage) {
-        console.log(this.warningStyle(`[WARNING]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.warningStyle("WARN"), strMessage));
     }
 
     // The function that logs a verbose statement, which is normally hidden
     logVerbose(strMessage) {
-        console.log(this.verboseStyle(`[VERBOSE]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.verboseStyle("VERB"), strMessage));
     }
 
     // The function that logs an OK message
     logOK(strMessage) {
-        console.log(this.okStyle(`[OK]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.okStyle("OKAY"), strMessage));
     }
 
     // The function that logs an informational statement
     logInfo(strMessage) {
-        console.log(this.infoStyle(`[INFO]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.infoStyle("INFO"), strMessage));
     }
 
     // The function that logs a success message
     logSuccess(strMessage) {
-        console.log(this.successStyle(`[SUCCESS]: `) + strMessage);
+        console.log(this._renderTheme("$DATE | $TIME | $TYPE | $MESSAGE", this.successStyle("SUCC"), strMessage));
     }
 
 }
