502f3b348375ea8134cba7f2e4b990785d6858f4
Vasanth Developer <vasanthdevelopers@gmail.com>
2019-01-18 13:37:37 +0000

    Created logCritical, logWarning, logVerbose, logOK, logInfo and logSuccess functions in LoggerClass class
    1. Created loggerClass.js script file
    2. Created createNewLogger function inside itivruttah.js and removed old demo function
    3. Removed shebang in itivruttah.js script file

--- itivruttah.js
+++ itivruttah.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 'use strict';
 
 // Require the loggerClass
--- loggerClass.js
+++ loggerClass.js
@@ -1,10 +1,54 @@
 'use strict';
 
+// Require the dependency node modules
+const dateformat = require('dateformat'),
+      chalk = require('chalk');
+
+
+// Create the loggerClass
 class LoggerClass {
 
     // The constructor
     constructor() {
-        console.log('It Works!');
+        // [TODO]: Take the args passed and make them global
+
+        // The chalk styles for different log types
+        this.criticalStyle = chalk.bold.redBright;
+        this.warningStyle = chalk.bold.yellowBright;
+        this.verboseStyle = chalk.bold.magentaBright;
+        this.okStyle = chalk.bold.gray;
+        this.infoStyle = chalk.bold.blueBright;
+        this.successStyle = chalk.bold.greenBright;
+    }
+
+    // The function that logs a critical statement, sometimes called a fatal or error
+    logCritical(strMessage) {
+        console.log(this.criticalStyle(`[ERROR]: `) + strMessage);
+    }
+
+    // The function that logs a warning statement
+    logWarning(strMessage) {
+        console.log(this.warningStyle(`[WARNING]: `) + strMessage);
+    }
+
+    // The function that logs a verbose statement, which is normally hidden
+    logVerbose(strMessage) {
+        console.log(this.verboseStyle(`[VERBOSE]: `) + strMessage);
+    }
+
+    // The function that logs an OK message
+    logOK(strMessage) {
+        console.log(this.okStyle(`[OK]: `) + strMessage);
+    }
+
+    // The function that logs an informational statement
+    logInfo(strMessage) {
+        console.log(this.infoStyle(`[INFO]: `) + strMessage);
+    }
+
+    // The function that logs a success message
+    logSuccess(strMessage) {
+        console.log(this.successStyle(`[SUCCESS]: `) + strMessage);
     }
 
 }
